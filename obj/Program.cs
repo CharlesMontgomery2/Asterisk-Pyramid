73e5c3525e5c1feffa2449eb98a8858f8dcd3c81
Charles Montgomery <charles.montgomery2@outlook.com>
2020-06-10 17:08:04 -0700

    Commit Stage

---
+++ obj/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asterisk_Pyramid
+{
+    class Program
+    {
+
+        static void AsteriskPyramid(int x)
+        {
+            for(int y=1; y<=x; y++)
+            {
+                for(int z=y; z<=x; z++)
+                {
+                    Console.Write("   ");
+                }
+                for(int k=1; k<=2*y-1; k++)
+                {
+                    Console.Write("*" + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            AsteriskPyramid(10);
+        }
+    }
+}
